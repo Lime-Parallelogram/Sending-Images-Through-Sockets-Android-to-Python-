6f5bc0f1a1057ec5a3e0079a17a0a906de943e94
Lime Parallelogram <Lime-Parallelogram@users.noreply.github.com>
2018-09-03 13:25:51 +0100

    Update MainActivity.java
    -Remove unnecessary lines
    -Add more comments

--- Client/MyApplication/app/src/main/java/com/limeparallelogram/imgtransmitproject/MainActivity.java
+++ Client/MyApplication/app/src/main/java/com/limeparallelogram/imgtransmitproject/MainActivity.java
@@ -2,8 +2,6 @@ package com.limeparallelogram.imgtransmitproject;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
@@ -12,21 +10,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.io.RandomAccessFile;
+
 import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
@@ -38,12 +27,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Set Up Button
         Button click = (Button)findViewById(R.id.button);
+        //Sets Up OnClick Listener For Button
         click.setOnClickListener(new View.OnClickListener() {
 
 
             @Override
             public void onClick(View v) {
+                //Activly Requests Acsess To Files (With Pop-Up)
                 if (ContextCompat.checkSelfPermission(MainActivity.this,
                         Manifest.permission.WRITE_EXTERNAL_STORAGE)
                         != PackageManager.PERMISSION_GRANTED) {
@@ -51,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
                             new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
                             STATUS_CODE);
                 } else {
+                    //Runs When Granted Permisssion
                     send sendcode = new send();
                     sendcode.execute();
 
@@ -62,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
     @Override
+    //More permission Granting code
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
         switch (requestCode) {
@@ -80,31 +74,31 @@ public class MainActivity extends AppCompatActivity {
 
 
 }
+//Main Actions - Asynchronous
 class send extends AsyncTask<Void,Void,Void> {
-    static Socket s;
+    static Socket s; //Socket Variable
     @Override
     protected Void doInBackground(Void...params){
         try {
-            s = new Socket("192.168.2.78",8000);
-            File directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-            File photoPath = new File(directory, "frog-face_1f438.png");
-            InputStream input = new FileInputStream(photoPath.getAbsolutePath());
+            s = new Socket("192.168.2.78",8000); //Connects to IP address - enter your IP here
+            File directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS); //Gets information about a said directory on your device - currently downloads
+            File photoPath = new File(directory, "frog-face_1f438.png"); //Define your image name I used png but other formats should also work - make sure to specify file extension on server
+            InputStream input = new FileInputStream(photoPath.getAbsolutePath()); //Gets the true path of your image
             try {
                 try {
+                    //Reads bytes (all together)
                     int bytesRead;
                     while ((bytesRead = input.read()) != -1) {
-                        ByteArrayOutputStream bout = new ByteArrayOutputStream();
-                        bout.write(bytesRead);
-                        s.getOutputStream().write(bout.toByteArray());
+                        s.getOutputStream().write(bytesRead); //Writes bytes to output stream
                     }
                 } finally {
+                    //Flushes and closes socket
                     s.getOutputStream().flush();
                     s.close();
                 }
             } finally {
                 input.close();
             }
-            FileReader fr = new FileReader(photoPath.getAbsolutePath());
 
 
         } catch (IOException e) {
